716cb2eaa7d988381e2e342eb4905f0922a23bbf
𝕯𝖎𝖑𝖓𝖚𝖗𝖆 <adilnura2197@gmail.com>
2026-04-22 17:00:33 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+class Kompyuter:
+    def yoq(self):
+        print("Kompyuter yoqildi")
+
+    def ishla(self):
+        self.yoq()
+        print("Kompyuter ishlamoqda")
+
+    def ochir(self):
+        self.ishla()
+        print("Kompyuter o‘chirildi")
